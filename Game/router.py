3911a97839b2c93715596c23eac7864170ea7ae7
nirow <gunmonyk@gmail.com>
2026-03-30 19:15:32 +0200

    feat: - add try except for all router functions

--- Game/router.py
+++ Game/router.py
@@ -31,9 +31,12 @@ async def choose_side(side: str, game_id: int, user: User = Depends(get_current_
 
 
 @router.post("/showdown/decision")
-async def cashout_decision(decision: str, game_id: int,  user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
+async def cashout_decision(decision: str, game_id: int, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
     game_service = GameService(session)
-    await game_service.set_showdown_decision(user.id, game_id, decision)
+    try:
+        await game_service.set_showdown_decision(user.id, game_id, decision)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     return {"message": "Cashout decision recorded successfully"}
 
 @router.get("/current", response_model=List[GameResponse])
@@ -45,13 +48,19 @@ async def get_current_games(user: User = Depends(get_current_user), session: Asy
 @router.get("/{game_id}/state")
 async def get_game_state(game_id: int, session: AsyncSession = Depends(get_session)):
     game_service = GameService(session)
-    state = await game_service.get_game_status(game_id)
+    try:
+        state = await game_service.get_game_status(game_id)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     return {"state": state}
 
 @router.get("/{game_id}/player")
-async def get_game_player(game_id: int, user: User =  Depends(get_current_user), session: AsyncSession = Depends(get_session)):
+async def get_game_player(game_id: int, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
     game_service = GameService(session)
-    player = await game_service.get_game_player(game_id, user.id)
+    try:
+        player = await game_service.get_game_player(game_id, user.id)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     return GamePlayerResponse.model_validate(player)
 
 @router.get("/admin/games")
