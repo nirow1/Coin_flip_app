cf4012cba054fe0940551435ba2b61500e7997af
nirow <gunmonyk@gmail.com>
2026-06-09 15:14:55 +0200

    feat: - add interfaces for register and login

---
+++ Frontend/Daily_flip/Src/Api/auth.ts
@@ -0,0 +1,21 @@
+import client from './client';
+
+export interface RegisterData{
+    username: string;
+    email: string;
+    password: string;
+    country: string;
+    dob: string; // ISO format: YYYY-MM-DD
+}
+
+export interface TokenResponse {
+    token: string;
+    token_type: string;
+};
+
+
+export const register = (data: RegisterData) =>
+  client.post('/auth/register', data);
+
+export const login = (username: string, password: string) =>
+  client.post('/auth/login', { username, password });
